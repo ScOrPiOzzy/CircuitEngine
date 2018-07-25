c7731c63e208e7644ad7e119dc0c48b3575b279f
张振宇 <sco_pra@126.com>
2018-07-25 10:30:16 +0800

    时间继电器

--- Circuit3D/engine/src/main/java/com/cas/circuit/element/TimeRelayElm.java
+++ Circuit3D/engine/src/main/java/com/cas/circuit/element/TimeRelayElm.java
@@ -1,17 +1,41 @@
 package com.cas.circuit.element;
 
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Function;
+
+import javax.xml.bind.Unmarshaller;
+
+import com.cas.circuit.component.Terminal;
+
 /**
  * 时间继电器
  * @author Administrator
  */
-public class TimeRelayElm extends RelayElmEx {
+public class TimeRelayElm extends RelayElm {
 
-	// 时间
-	private float time = 10;
-	// 单位
-	private int unit;
-	
-//	TODO
+	// 设定值, 默认10秒
+	private float preset = 10;
+//	// 量程:如10s、100s、10min、60min
+//	private int range = 10;
+
+	private float count = 0;
+
+	private Timer timer;
+
+	public TimeRelayElm() {
+		super();
+	}
+
+	public TimeRelayElm(Unmarshaller u, Function<String, Terminal> f, Map<String, String> params) {
+		super(u, f, params);
+
+		String value = params.get("preset");
+		preset = value == null ? preset : Integer.parseInt(value);
+	}
+
+	// TODO
 	@Override
 	public void startIteration() {
 		if (force) {
@@ -32,7 +56,13 @@ public class TimeRelayElm extends RelayElmEx {
 			}
 		} else if (Math.abs(delta - coilCurrent) < 1e-8) {
 			if (lock) {
-				button.unstuck();
+
+				count = 0;
+				if (timer != null) {
+					timer.cancel();
+					timer = null;
+				}
+
 				lock = false;
 			}
 		}
@@ -40,19 +70,38 @@ public class TimeRelayElm extends RelayElmEx {
 		if (d_position > 1) {
 			d_position = 1;
 			if (!lock) {
+				startCount();
+
 				lock = true;
-				button.absorbed();
 			}
 		}
 
 		if (d_position < .1) {
 			i_position = 0;
 		} else if (d_position > .9) {
-			i_position = 1;
+			if (count >= preset) {
+				i_position = 1;
+				timer.cancel();
+			}
 		} else {
 			i_position = 2;
 		}
 		delta = coilCurrent;
+	}
+
+	private void startCount() {
+		if (timer != null) {
+			timer.cancel();
+		}
+		timer = new Timer();
+		count = 0;
+		timer.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				count += 0.1;
+			}
+		}, 0, 100);
 	}
 
 }
